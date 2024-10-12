613f7d0cad5d2b0e2c5763f026a33d2f3728641d
Oliver Bramich <147389067+Bramich04@users.noreply.github.com>
2024-10-12 15:30:35 +1100

    Add files via upload

---
+++ Visualisation_II.js
@@ -0,0 +1,20 @@
+var vg_1 = "TemperatureMap.vega";
+vegaEmbed("#TemperatureMap", vg_1, {"actions": false}).then(function(result) {}).catch(console.error);
+
+var vg_2 = "BushfireMap.vega";
+vegaEmbed("#BushfireMap", vg_2, {"actions": false}).then(function(result) {}).catch(console.error);
+
+var vg_3 = "EnvironmentBubbleplot.vega";
+vegaEmbed("#EnvironmentBubbleplot", vg_3, {"actions": false}).then(function(result) {}).catch(console.error);
+
+var vg_4 = "LandStackedbarcart.vega";
+vegaEmbed("#LandStackedbarcart", vg_4, {"actions": false}).then(function(result) {}).catch(console.error);
+
+var vg_5 = "AnimalclassDonut.vega";
+vegaEmbed("#AnimalclassDonut", vg_5, {"actions": false}).then(function(result) {}).catch(console.error);
+
+var vg_6 = "AnimalspeciesDonut.vega";
+vegaEmbed("#AnimalspeciesDonut", vg_6, {"actions": false}).then(function(result) {}).catch(console.error);
+
+var vg_7 = "BushfireLinechart.vega";
+vegaEmbed("#BushfireLinechart", vg_7, {"actions": false}).then(function(result) {}).catch(console.error);
